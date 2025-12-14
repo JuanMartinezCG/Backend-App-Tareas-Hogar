5fb065d7daa75ab80ea435b25562a6ed88548739
juanMartinez <oo199@outlook.com>
2025-12-14 12:11:12 -0500

    inicio de la creacion logout

---
+++ Backend App Tareas Hogar/Application/Users/Logout/LogoutCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Backend_App_Tareas_Hogar.Application.Users.Logout
+{
+    public class LogoutCommand : IRequest<LogoutResponse>
+    {
+        public string RefreshToken { get; set; }
+    }
+
+}
---
+++ Backend App Tareas Hogar/Application/Users/Logout/LogoutHandler.cs
@@ -0,0 +1,31 @@
+﻿using Backend_App_Tareas_Hogar.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Backend_App_Tareas_Hogar.Application.Users.Logout
+{
+    public class LogoutHandler
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public LogoutHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<LogoutResponse> Handle(LogoutCommand request, CancellationToken cancellationToken)
+        {
+            // Buscar el refresh token en la base de datos
+            var token = await _dbContext.UserTokens 
+                .FirstOrDefaultAsync(t => t.RefreshToken == request.RefreshToken, cancellationToken);
+
+            if (token == null)
+                return LogoutResponse.NotFound();
+
+            token.IsRevoked = true;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return LogoutResponse.Done();
+        }
+    }
+}
