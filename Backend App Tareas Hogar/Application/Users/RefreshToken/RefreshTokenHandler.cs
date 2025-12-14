ea008191542e41f4a3013d7ff90f0446c769f700
juanMartinez <oo199@outlook.com>
2025-12-14 12:13:01 -0500

    creacion del refreshToken

---
+++ Backend App Tareas Hogar/Application/Users/RefreshToken/RefreshTokenHandler.cs
@@ -0,0 +1,27 @@
+﻿using Backend_App_Tareas_Hogar.Application.Users.Login;
+using Backend_App_Tareas_Hogar.Infraestructure.Interfaces;
+using MediatR;
+
+namespace Backend_App_Tareas_Hogar.Application.Users.RefreshToken
+{
+    public class RefreshTokenHandler : IRequestHandler<RefreshTokenCommand, RefreshTokenResponse>
+    {
+        private readonly IJwtService _jwtService;
+        public RefreshTokenHandler(IJwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        public async Task<RefreshTokenResponse> Handle(
+            RefreshTokenCommand request,
+            CancellationToken cancellationToken)
+        {
+            var tokenNew = await _jwtService.RefreshTokenAsync(request.RefreshToken);
+            
+            if(tokenNew.Token == null)
+                return RefreshTokenResponse.Invalid(tokenNew.Message);
+
+            return RefreshTokenResponse.Success(tokenNew.Token, tokenNew.Refreshtoken);
+        }
+    }
+}
